732c1a83770d53dc1caada81fd38fa6ebc358033
zaquari_andl <zaquariandl043@gmail.com>
2023-11-15 17:35:43 -0600

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+const replace = document.getElementById('text')
+document.addEventListener("DOMContentLoaded", function() {
+    replace.textContent = "This is really cool!"
+});
